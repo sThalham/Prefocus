9c273a8f6e88a7c42865a877d020d80c16525f1c
Your Name <you@example.com>
2022-09-10 14:00:13 +0200

    some placeholder

--- model.py
+++ model.py
@@ -1,50 +1,55 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class CAOS_Model(nn.Module):
-    def __init__(self, backbone_model, num_classes, box_points):
-        super(CAOS_Model, self).__init__()
+from BiFPN import BiFPN
+
+class Prefocus_Model(nn.Module):
+    def __init__(self, backbone_model, num_classes):
+        super(Prefocus_Model, self).__init__()
         self.backbone = backbone_model()
-        self.FPN = FeaturePyramid()
+        self.FPN = BiFPN([sizes])
         self.dis = FBDiscriminator()
         self.cls = ClassificationHead(num_classes)
-        self.box = BoxHead(box_points)
+        self.box = BoxHead(4)
         self.cub = CuboidHead(8)
 
     def forward(self, x):
         C3, C4, C5 = self.backbone(x)
         P3, P4, P5, P6, P7 = self.FPN(C3, C4, C5)
 
-        P3_size = P3.size()
-        P4_size = P4.size()
-        P5_size = P5.size()
-        P6_size = P6.size()
-        P7_size = P7.size()
+        maps = [P3, P4, P5, P6, P7]
+        fm_sizes = [P3.size(), P4.size(), P5.size(), P6.size(), P7.size()]
 
-        # discriminator
-        dis_P3 = self.dis(P3)
-        dis_P3 = dis_P3.view(P3_size[0], P3_size[1], P3_size[2] * P3_size[3])
+        locs = []
+        clss = []
+        boxs = []
+        cubs = []
+        for lv, map in enumerate(maps):
+            #discriminator
+            dis_level = self.dis(map)
+            dis_level = dis_level.view(fm_sizes[lv][0], fm_sizes[lv][1], fm_sizes[lv][2] * fm_sizes[lv][3])
+            locs.append(dis_level)
+            # classes
+            cls_level = self.cls(map)
+            cls_level = cls_level.view(fm_sizes[lv][0], fm_sizes[lv][1], fm_sizes[lv][2] * fm_sizes[lv][3])
+            clss.append(cls_level)
+            # boxes
+            box_level = self.box(map)
+            box_level = box_level.view(fm_sizes[lv][0], fm_sizes[lv][1], fm_sizes[lv][2] * fm_sizes[lv][3])
+            boxs.append(box_level)
+            # cuboids
+            cub_level = self.cub(map)
+            cub_level = cub_level.view(fm_sizes[lv][0], fm_sizes[lv][1], fm_sizes[lv][2] * fm_sizes[lv][3])
+            cubs.append(cub_level)
 
+        locations = torch.cat(locs, dim=2)
+        classes = torch.cat(clss, dim=2)
+        boxes = torch.cat(boxs, dim=2)
+        cuboids = torch.cat(cubs, dim=2)
 
-class FeaturePyramid(nn.Module):
-    def __init__(self):
-        super(FBDiscriminator, self).__init__()
-        self.conv1 = nn.Conv2d(256, 512, 3)
-        self.conv2 = nn.Conv2d(512, 256, 3)
-        self.conv3 = nn.Conv2d(256, 128, 3)
-        self.conv4 = nn.Conv2d(128, 64, 3)
-        self.conv5 = nn.Conv2d(64, 1, 3)
-
-    def forward(self, P3, P4, P5):
-        #x = x.view(-1, 16 * 4 * 4)
-        x = F.mish(self.fc1(x))
-        x = F.mish(self.fc2(x))
-        x = F.mish(self.fc3(x))
-        x = F.mish(self.fc4(x))
-        x = F.sigmoid(self.fc5(x))
-
-        return x
+        return [locations, classes, boxes, cuboids]
 
 
 class FBDiscriminator(nn.Module):
--- train.py
+++ train.py
@@ -1,10 +1,103 @@
 import torch
 
 from backbone import backbone_model
-from model import CAOS_Model
+from model import Prefocus_Model
 
 
-def create_model():
+def create_model(bn_name, num_classes):
     bb_model = backbone_model('resnext50')
-    model = CAOS_Model(bb_model)
+    model = Prefocus_Model(bb_model, num_classes=num_classes)
     return model
+
+
+def train_one_epoch(epoch_index, tb_writer):
+    running_loss = 0.
+    last_loss = 0.
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(training_loader):
+        # Every data instance is an input + label pair
+        inputs, labels = data
+
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        outputs = model(inputs)
+
+        # Compute the loss and its gradients
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000 # loss per batch
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            tb_x = epoch_index * len(training_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            running_loss = 0.
+
+    return last_loss
+
+
+def main():
+
+    backbone_name = 'resnext50'
+    num_classes = 13
+    model = create_model(backbone_name, num_classes)
+
+    ce_loss_fn = torch.nn.CrossEntropyLoss()
+    nll_loss_fn = torch.nn.NLLLoss()
+    sl1_loss_fn = torch.nn.SmoothL1Loss()
+
+    optimizer = torch.optim.SGD(model.parameters())
+
+    # Initializing in a separate cell so we can easily add more epochs to the same run
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    writer = SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
+    epoch_number = 0
+
+    EPOCHS = 5
+
+    best_vloss = 1_000_000.
+
+    for epoch in range(EPOCHS):
+        print('EPOCH {}:'.format(epoch_number + 1))
+
+    # Make sure gradient tracking is on, and do a pass over the data
+    model.train(True)
+    avg_loss = train_one_epoch(epoch_number, writer)
+
+    # We don't need gradients on to do reporting
+    model.train(False)
+
+    running_vloss = 0.0
+    for i, vdata in enumerate(validation_loader):
+        vinputs, vlabels = vdata
+    voutputs = model(vinputs)
+    vloss = loss_fn(voutputs, vlabels)
+    running_vloss += vloss
+
+    avg_vloss = running_vloss / (i + 1)
+    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+    # Log the running loss averaged per batch
+    # for both training and validation
+    writer.add_scalars('Training vs. Validation Loss',
+                       {'Training': avg_loss, 'Validation': avg_vloss},
+                       epoch_number + 1)
+    writer.flush()
+
+    # Track best performance, and save the model's state
+    if avg_vloss < best_vloss:
+        best_vloss = avg_vloss
+    model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+    torch.save(model.state_dict(), model_path)
+
+    epoch_number += 1
